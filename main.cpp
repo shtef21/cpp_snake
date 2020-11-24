ca8b81c820a43f360b034e9e5965e15e8e4acbd7
shtef21 <72214442+shtef21@users.noreply.github.com>
2020-11-24 20:21:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-
-// Remove file
-#include <stdio.h>
-
 #include "App"
 
 int main(int argc, char *argv[])
 {
     App::run();
-
-    // Clean directory
-    remove("main.obj");
     return 0;
 }
